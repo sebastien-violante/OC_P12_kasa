060124bbf8d600afbef6fd9c686a84d8ae6a151c
sebastien-violante <sebastien.violante@gmail.com>
2026-09-25 18:30:06 +0200

    poursuite tests connexion

--- src/app/connexion/page.test.tsx
+++ src/app/connexion/page.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Connexion from "./page";
 import userEvent from "@testing-library/user-event";
+import postRequest from "../utils/postRequest";
 
 // MOCK DES IMPORTS ///////////////////////////////////
 
@@ -18,8 +19,16 @@ jest.mock("../context/AuthContext", () => ({
   }),
 }));
 
+jest.mock("../utils/postRequest", () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+// TESTS ////////////////////////////////////////////////
+
 describe("connexion", () => {
-  it("affichage du formulaire", () => {
+  // ✅ formulaire affiché
+  it("affiche le formulaire", () => {
     render(<Connexion />);
     const emailInput = screen.getByLabelText(/Adresse email/i);
     const passwordInput = screen.getByLabelText(/Mot de passe/i);
@@ -32,6 +41,7 @@ describe("connexion", () => {
     expect(passwordInput).toBeInTheDocument();
   });
 
+  // ✅ champs remplissables
   it("permet à l'utilisateur de remplir le formulaire", async () => {
     const user = userEvent.setup();
     render(<Connexion />);
@@ -49,15 +59,86 @@ describe("connexion", () => {
     expect(passwordInput).toHaveValue("P@swworD123");
   });
 
-  it("soulève des erreurs si les champs sont vides", async() => {
+  // ✅ validation champs vides
+  it("soulève des erreurs si les champs sont vides et place les inputs en aria-invalid", async () => {
     const user = userEvent.setup();
     render(<Connexion />);
+    const emailInput = screen.getByLabelText(/Adresse email/i);
+    const passwordInput = screen.getByLabelText(/Mot de passe/i);
     const submitButton = screen.getByRole("button", {
-      name: /Se connecter/i
-    })
+      name: /Se connecter/i,
+    });
 
-    await user.click(submitButton)
-    expect(await screen.findByText(/L'adresse email est requise/i)).toBeInTheDocument()
-    expect(await screen.findByText(/Le mot de passe est requis/i)).toBeInTheDocument()
-  })
+    expect(emailInput).toHaveAttribute("aria-invalid", "false");
+    expect(passwordInput).toHaveAttribute("aria-invalid", "false");
+    await user.click(submitButton);
+    expect(
+      await screen.findByText(/L'adresse email est requise/i),
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByText(/Le mot de passe est requis/i),
+    ).toBeInTheDocument();
+    expect(emailInput).toHaveAttribute("aria-invalid", "true");
+    expect(passwordInput).toHaveAttribute("aria-invalid", "true");
+  });
+
+  // ✅ validation email invalide
+  it("affiche une erreur lorsque l'email est invalide", async () => {
+    const user = userEvent.setup();
+    render(<Connexion />);
+    const emailInput = screen.getByLabelText(/Adresse email/i);
+    const passwordInput = screen.getByLabelText(/Mot de passe/i);
+    const submitButton = screen.getByRole("button", {
+      name: /Se connecter/i,
+    });
+
+    await user.type(emailInput, "john.doe");
+    await user.type(passwordInput, "P@swworD123");
+    await user.click(submitButton);
+
+    expect(
+      await screen.findByText(/Le format de l'email est invalide/i),
+    ).toBeInTheDocument();
+    expect(
+      screen.queryByText(/Le mot de passe est requis/i),
+    ).not.toBeInTheDocument();
+  });
+
+  // ✅ API appelée et avec les bonnes données
+  it("fetch l'API pour vérifier les données de connexion", async () => {
+    const user = userEvent.setup();
+    render(<Connexion />);
+    const emailInput = screen.getByLabelText(/Adresse email/i);
+    const passwordInput = screen.getByLabelText(/Mot de passe/i);
+    const submitButton = screen.getByRole("button", {
+      name: /Se connecter/i,
+    });
+    const mockedPostRequest = jest.mocked(postRequest);
+    const mockedValue = {
+      data: {
+        token: "fake-token",
+        user: {
+          id: 123,
+          name: "john Doe",
+          picture: "fake-src",
+        },
+      },
+      success: true,
+      message: "authentification réussie",
+    };
+    mockedPostRequest.mockResolvedValue(mockedValue);
+
+    await user.type(emailInput, "john.doe@gmail.com");
+    await user.type(passwordInput, "P@swworD123");
+    await user.click(submitButton);
+    expect(mockedPostRequest).toHaveBeenCalledWith(
+      expect.objectContaining({
+        url: expect.stringContaining("/auth/login"),
+        payload: {
+          email: "john.doe@gmail.com",
+          password: "P@swworD123",
+        },
+      }),
+    );
+  });
 });
